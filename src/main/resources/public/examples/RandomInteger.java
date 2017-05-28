8c4c3ccfeab3aca4c178a9682e8f7c99650ba724
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-28 13:30:51 +0100

    Minor change

--- src/main/resources/public/examples/RandomInteger.java
+++ src/main/resources/public/examples/RandomInteger.java
@@ -2,18 +2,18 @@ import java.util.Random;
 
 /**
  * This example show how to generate Random Numbers.
- *
  * */
 public class RandomInteger {
   
-  public static void main(String[] aArgs){
+  public static void main(final String[] args) {
 
     System.out.println("Generating 10 random numbers.");
 
-    Random randomGenerator = new Random();
+    int randomNumber = 0;
+    final Random randomGenerator = new Random();
     for (int x = 1; x <= 10; x++) {
-      int randomInt = randomGenerator.nextInt(100);
-      System.out.println("Generated : " + randomInt);
+      randomNumber = randomGenerator.nextInt(100);
+      System.out.println("Generated : " + randomNumber);
     }
 
     System.out.println("Done.");
