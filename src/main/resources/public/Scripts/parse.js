d81ce96dca75483205baab9fd75a53244ab20225
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-28 12:05:14 +0100

    Adding initial support for Combos

--- src/main/resources/public/Scripts/parse.js
+++ src/main/resources/public/Scripts/parse.js
@@ -32,7 +32,39 @@
         }, delay || 811);
     }
 
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
     window.onload = function () {
+
+        var example = getUrlParameter('example');
+        console.log(example);
+        if(example == undefined){
+            example = "HelloWorld.java";
+        }
+
+        $.ajax({
+            url : "/examples/" + example,
+            dataType: "text",
+            success : function (data) {
+                //$(".text").html(data);
+                //console.log(data);
+                $("#editor").html(data);
+            }
+        });
+
         require(["orion/editor/edit"], function(edit) {
             window.editor = edit({className: "editor"});
             window.editor.getTextView().getModel().addEventListener("Changed", function () { parse(); });
---
+++ src/main/resources/public/examples/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Hello World, is your first program in Java language.
+ *
+ * @author jabrena
+ */
+public class HelloWorld {
+
+    /**
+     * The Java Virtual Machine starts up by loading a specified class
+     * and then invoking the method main in this specified class.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
