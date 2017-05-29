89423dfa7bab40596961ea619410c101791b4e04
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-29 17:20:44 +0100

    Updates for robotics section

--- src/main/resources/public/Scripts/parse.js
+++ src/main/resources/public/Scripts/parse.js
@@ -1,5 +1,20 @@
 "use strict";
 
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
 (function() {
 
     var parseId;
@@ -32,20 +47,7 @@
         }, delay || 811);
     }
 
-    var getUrlParameter = function getUrlParameter(sParam) {
-        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-            sURLVariables = sPageURL.split('&'),
-            sParameterName,
-            i;
 
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
-
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === undefined ? true : sParameterName[1];
-            }
-        }
-    };
 
     window.onload = function () {
 
